d8425d708da428ebb8fbfb46b5ebcb34e4a79698
JungBear <mod459@naver.com>
2022-08-17 15:39:10 +0900

    updated

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "57"
+    return "59"
 
 # 사용자가 공고를 보기 원할 때 
 @app.route("/api/anninputloc", methods=["post"])
@@ -60,65 +60,94 @@ def sayHello():
 @app.route('/api/annout', methods=['POST'])
 def location():
     body = request.get_json()
-    print(body)
+    #print(body)
     params_df=body['action']['params']
-    print(params_df)
+    #print(params_df)
    
     loc=params_df['loc']
-    print(loc)
-    print(type(loc))
+    #print(loc)
+    #print(type(loc))
 
     loc_li="'%" + loc + "%'"
     df1=start.db_select(loc_li)
-    print(df1)
+    #print(df1)
     name=df1['name']
-    print(name)
-    print(type(name))
+    #print(name)
+    #print(type(name))
     URL = df1['rink']
-    responseBody = {
-        "version": "2.0",
-        "template": {
-            "outputs": [
-                {
-                    "listCard": {
-                        "header": {
-                            "title": "공고 내역입니다."
-                        },
-                        "items": [
-                            {
-                                "title": name[0],
-                                "imageUrl": "http://k.kakaocdn.net/dn/APR96/btqqH7zLanY/kD5mIPX7TdD2NAxgP29cC0/1x1.jpg",
-                                "link": {
-                                    "web": URL[0]
-                                }
+    if len(df1) > 0:
+        responseBody = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "listCard": {
+                            "header": {
+                                "title": "공고 내역입니다."
                             },
-                            {
-                                "title": name[1],
-                                "imageUrl": "http://k.kakaocdn.net/dn/N4Epz/btqqHCfF5II/a3kMRckYml1NLPEo7nqTmK/1x1.jpg",
-                                "link": {
-                                    "web": URL[1]
+                            "items": [
+                                {
+                                    "title": name[0],
+                                    "imageUrl": "http://k.kakaocdn.net/dn/APR96/btqqH7zLanY/kD5mIPX7TdD2NAxgP29cC0/1x1.jpg",
+                                    "link": {
+                                        "web": URL[0]
+                                    }
+                                },
+                                {
+                                    "title": name[1],
+                                    "imageUrl": "http://k.kakaocdn.net/dn/N4Epz/btqqHCfF5II/a3kMRckYml1NLPEo7nqTmK/1x1.jpg",
+                                    "link": {
+                                        "web": URL[1]
+                                    }
+                                },
+                                {
+                                    "title": name[2],
+                                    "imageUrl": "http://k.kakaocdn.net/dn/bE8AKO/btqqFHI6vDQ/mWZGNbLIOlTv3oVF1gzXKK/1x1.jpg",
+                                    "link": {
+                                        "web": URL[2]
+                                    }
                                 }
-                            },
-                            {
-                                "title": name[2],
-                                "description": "보이스봇 / KVS 제휴 신청하기",
-                                "imageUrl": "http://k.kakaocdn.net/dn/bE8AKO/btqqFHI6vDQ/mWZGNbLIOlTv3oVF1gzXKK/1x1.jpg",
-                                "link": {
-                                    "web": URL[2]
+                            ],
+                            "buttons": [
+                                {
+                                    "label": "더보기",
+                                    "action": "webLink",
+                                    "webLinkUrl": "https://www.applyhome.co.kr/ai/aia/selectAPTLttotPblancListView.do#"
                                 }
-                            }
-                        ],
-                        "buttons": [
-                            {
-                                "label": "더보기",
-                                "action": "webLink",
-                                "webLinkUrl": "https://www.applyhome.co.kr/ai/aia/selectAPTLttotPblancListView.do#"
-                            }
-                        ]
+                            ]
+                        }
                     }
-                }
-            ]
+                ]
+            }
         }
-    }
+    else :
+        {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "listCard": {
+                            "header": {
+                                "title": "공고 내역입니다."
+                            },
+                            "items": [
+                                {
+                                    "title": '죄송합니다 해당지역공고가 없습니다.',
+                                },
+                            ],
+                            "buttons": [
+                                {
+                                    "label": "다른공고더보기",
+                                    "action": "webLink",
+                                    "webLinkUrl": "https://www.applyhome.co.kr/ai/aia/selectAPTLttotPblancListView.do#"
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        }
 
+
+    
     return responseBody
