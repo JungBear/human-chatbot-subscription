d12b67933462e4f7aebae8782f6b73e7627d60c0
JungBear <mod459@naver.com>
2022-08-17 10:17:53 +0900

    updated

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "1234444"
+    return "asdfasdf"
 
 # 사용자가 공고를 보기 원할 때 
 @app.route("/api/anninputloc", methods=["post"])
@@ -67,7 +67,7 @@ def location():
     print(loc)
     print(type(loc))
 
-    loc_li="%'" + loc + "%'"
+    loc_li="'%" + loc + "%'"
     df1=start.db_select(loc_li)
     print(df1)
     name=df1['Name']
